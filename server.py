5f9cac04559cf22754a54d9d0298dc1b2ba2643a
chiabhoward <b06902039@ntu.edu.tw>
2020-06-02 21:29:33 +0800

    modify tab and '__main__'

--- server.py
+++ server.py
@@ -3,36 +3,36 @@ import threading
 import json
 class thread_accept_client(threading.Thread):
     def __init__(self, listen_socket, client_list):
-        super(thread_listen_client, self)
-        self._stop_event = threding.Event() #For Stoping Thread
+        super(thread_accept_client, self)
+        self._stop_event = threading.Event() #For Stoping Thread
         self.socket = listen_socket
         self.list = client_list
-        with open('db.json', 'wr', encoding='utf-8') as f:
+        with open('db.json', 'r+', encoding='utf-8') as f:
             self.database = json.load(f)
 
     def run(self):
-	    print(u'waiting for connect...')
-	    while(self._stop_event.is_set() == False):
-    		#Todo: client info maintain
-    		connect, (host, port) = self.socket.accept()
-    		print(u'the client %s:%s has connected.' % (host, port))
-    		recv_data = self.socket.recv(1024)
-                recv_data = recv_data.decode('utf-8').split(',')
-                if self.database[recv_data[0]] is recv_data[1]: #Check if user is valid
-                    self.list.append({"host":host,
+        print(u'waiting for connect...')
+        while(self._stop_event.is_set() == False):
+            #Todo: client info maintain
+            connect, (host, port) = self.socket.accept()
+            print(u'the client %s:%s has connected.' % (host, port))
+            recv_data = self.socket.recv(1024)
+            recv_data = recv_data.decode('utf-8').split(',')
+            if self.database[recv_data[0]] is recv_data[1]: #Check if user is valid
+                self.list.append({"host":host,
                                   "port":port,
                                   "usrname":recv_data[0],
                                   "passwd":recv_data[1],
                                   "Socket":connect
                                  })
-	            connect.sendall(b'ACK')
-	        else:
-                    connect.sendall(b'NEG')
+                connect.sendall(b'ACK')
+            else:
+                connect.sendall(b'NEG')
         
     def stop(self):
         self._stop_event.set()
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     
     #=====Create Socket to listen=====
     IP = '127.0.0.1'
@@ -51,7 +51,7 @@ if "__name__" == "__main__":
               "Socket":connect}
             ]
     '''
-	
+    
     #======Start Listening======
     listening = thread_accept_client(server, client_list)
     listening.start()
@@ -66,8 +66,8 @@ if "__name__" == "__main__":
             connect.sendall(b'your words has received.')
             print(b'the client say:' + data)
      
-	 
-	listening.stop()
-	listening.join()
-	 
+     
+    listening.stop()
+    listening.join()
+     
     server.close()
