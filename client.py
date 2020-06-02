3d5c54cdbd04df529893113829b54a21aa800ebf
chiabhoward <b06902039@ntu.edu.tw>
2020-06-02 21:21:35 +0800

    modify '__main__'

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import threading
 class thread_recv_sound(threading.Thread):
     def __init__(self, client_socket):
         super(thread_recv_sound, self).__init__
-        self._stop_event = threding.Event() #For Stoping Thread
+        self._stop_event = threding.Event() #For Stopping Thread
         self.socket = client_socket
 
     def run(self):
@@ -20,28 +20,29 @@ class thread_recv_sound(threading.Thread):
         self._stop_event.set()
 
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     #======Connect to Server=====
     IP = '127.0.0.1'
     port = 9999
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((IP, port)) #Server IP & Port
-	
+    
     #====Check username & password====
-	confirm = 0
-	while(not confirm):
-	    #===========Loggin============
-            Username = input('Please enter your name: ')
-            Password = input('Password: ')
-	    data = Username + ',' + Password + '\n'
-	    data = data.encode('utf-8')
-            client.sendall(data)
-	    data = client.recv(1024)
-	    if(data == b'ACK'):
-	        print("Loggin Success!!")
-		confirm = 1
-	    else:
-	        print("Username or password isn\'t correct.")
+    confirm = 0
+    while not confirm:
+        print('Now Login')
+       #===========Loggin============
+        Username = input('Please enter your name: ')
+        Password = input('Password: ')
+        data = Username + ',' + Password + '\n'
+        data = data.encode('utf-8')
+        client.sendall(data)
+        data = client.recv(1024)
+        if(data == b'ACK'):
+            print("Loggin Success!!")
+            confirm = 1
+        else:
+            print("Username or password isn't correct.")
     
     #======Create Thread to Recieve msg======
     recv_sound = thread_recv_sound(client)
