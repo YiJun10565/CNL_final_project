8bbaf98009890b7bc2b2b1dc5fbbaeb37ef9bda4
B06902128 <b06902128@ntu.edu.tw>
2020-06-02 18:45:46 +0800

    Todo

--- client.py
+++ client.py
@@ -1,19 +1,49 @@
 import socket
+import threading
+
+class thread_recv_sound(threading.Thread):
+    def __init__(self, client_socket):
+        super(thread_recv_sound, self).__init__
+        self._stop_event = threding.Event() #For Stoping Thread
+        self.socket = client_socket
+
+    def run(self):
+        while(self._stop_event.is_set() == False):
+            recv_data = self.socket.recv(1024)
+            '''
+            Processing Data....
+
+            Todo:
+
+            '''
+    def stop(self):
+        self._stop_event.set()
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 6688))
+if "__name__" == "__main__":
+    #===========Login============
+    Username = input('Please enter your name: ')
+    Password = input('Password: ')
 
-while True:
-    data = input()
-    data = data.encode('utf-8')
-    if data == b'quit':
-        print(b'connect quit.')
-        break
-    else:
+    #======Connect to Server=====
+    IP = '127.0.0.1'
+    port = 9999
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((IP, port)) #Server IP & Port
+    #Todo: Check username & password
+
+    
+    #======Create Thread to Recieve msg======
+    recv_sound = thread_recv_sound(client)
+    recv_sound.start()
+
+    while True: #Todo: check authorization to talk
+        data = input() #Change this to vocal
+        data = data.encode('utf-8')
         client.sendall(data)
-        rec_data = client.recv(1024)
-        print(b'form server receive:' + rec_data)
 
-client.sendall(b'quit')
-client.close()
+    recv_sound.stop()
+    recv_sound.join()
+
+    client.sendall(b'quit')
+    client.close()
--- server.py
+++ server.py
@@ -1,22 +1,50 @@
 import socket
+import threading
 
+class thread_accept_client(threading.Thread):
+    def __init__(self, listen_socket):
+        super(thread_listen_client, self)
+		self._stop_event = threding.Event() #For Stoping Thread
+        self.socket = listen_socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 6688))
-server.listen(5)
+    def run(self):
+	    print(u'waiting for connect...')
+		while(self._stop_event.is_set() == False):
+		    #Todo: client info maintain
+    		connect, (host, port) = server.accept()
+            print(u'the client %s:%s has connected.' % (host, port))
+        
+    def stop(self):
+        self._stop_event.set()
 
-print(u'waiting for connect...')
-connect, (host, port) = server.accept()
-print(u'the client %s:%s has connected.' % (host, port))
+if '__name__' == '__main__':
+    
+    #=====Create Socket to listen=====
+    IP = '127.0.0.1'
+    Port = 9999
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP, Port))
+    server.listen(5)
 
-while True:
-    data = connect.recv(1024)
-    if data == b'quit' or data == b'':
-        print(b'the client has quit.')
-        break
-    else:
-        connect.sendall(b'your words has received.')
-        print(b'the client say:' + data)
+    #======client data======== (Todo)
+	
+	
+	#======Start Listening======
+	listening = thread_accept_client(server)
+	listening.start()
+    
 
-server.close()
-
+    while True:
+        data = connect.recv(1024)
+        if data == b'quit' or data == b'':
+            print(b'the client has quit.')
+            break
+        else:
+             connect.sendall(b'your words has received.')
+             print(b'the client say:' + data)
+     
+	 
+	 listening.stop()
+	 listening.join()
+	 
+     server.close()
