534d5402210d38adb177846e4551abcb01e9c47f
b06902033 <b06902033@csie.ntu.edu.tw>
2020-06-06 15:21:00 +0800

    add clients info into it

--- Variables.py
+++ Variables.py
@@ -5,3 +5,16 @@ class States:
     waiting_for_talk = "Waiting for talk"
     talking = "Talking"
 
+class Client_info():
+    def __init__(self, connect, host, port):
+        self.connect = connect
+        self.host = host
+        self.port = port
+        self.username = ""
+        self.password  = ""
+        self.state = States.initial
+    def print_info(self):
+        print("Connect to %s:%d" % (self.host, self.port))
+        print("Current state: %s" % self.state)
+        print("Username = %s" % self.username)
+        print("Password = %s" % self.password)
